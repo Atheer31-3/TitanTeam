1dc873340a5808a68978830ac03e5963f7119e0c
Deem Ibrahim Alfaleh <dfalfaleh@gmail.com>
2024-09-30 16:15:14 +0300

    new edit

--- TitanTeam/TitanTeam/Schedule.swift
+++ TitanTeam/TitanTeam/Schedule.swift
@@ -9,9 +9,37 @@ import SwiftUI
 
 struct Schedule: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+        VStack(alignment: .leading) {
+            HStack {
+                Image(systemName: "person.circle")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                
+                Spacer(minLength: 100)
+                
+                Image(systemName: "bell")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+            }
+            HStack{
+                DatePicker(selection: /*@START_MENU_TOKEN@*/.constant(Date())/*@END_MENU_TOKEN@*/, label: {  })
+            }
+            .padding()
+            Spacer()
+        }
+            
+        
+            
+            //        Table(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=Collection@*/ /*@END_MENU_TOKEN@*/) {
+            //            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/ /*@END_MENU_TOKEN@*/
+            //        }
+            Text("Genius is one percent inspiration, 99% perspiration \n")
+            Image(systemName: "plus.circle").resizable()
+            .frame(width: 50, height: 50)
+            
+        }
     }
-}
 
 #Preview {
     Schedule()
