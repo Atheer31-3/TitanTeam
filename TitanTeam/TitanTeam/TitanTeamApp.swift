829e828922da7880a27b7f215fe1377b266609df
Atheer Alshareef <105993810+Atheer31-3@users.noreply.github.com>
2024-09-30 14:42:02 +0300

    first commit

---
+++ TitanTeam/TitanTeam/TitanTeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  TitanTeamApp.swift
+//  TitanTeam
+//
+//  Created by atheer alshareef on 27/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct TitanTeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
