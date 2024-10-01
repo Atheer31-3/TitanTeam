4138339c38984101c487f50cbc6056807a554639
Atheer Alshareef <105993810+Atheer31-3@users.noreply.github.com>
2024-10-01 23:19:40 +0300

    sitting

--- TitanTeam/TitanTeam/Sitting.swift
+++ TitanTeam/TitanTeam/Sitting.swift
@@ -6,101 +6,104 @@ struct Sitting: View {
     @State private var notificationsOn: Bool = false
     @State private var showAboutUs: Bool = false
     @State private var name: String = ""
-//    let scrums: [Sitting]
     
     var body: some View {
-        HStack {
-            Button(action: {
-                // Back action
-            }) {
-                Image(systemName: "chevron.left")
-                    .foregroundColor(.blue)
-                Text("schedule")
-                    .foregroundColor(.blue)
-            }
-            Spacer()
-        }
-        .padding()
-
-        Text("Sitting")
-            .font(.system(size: 40, weight: .bold))
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.horizontal)
-        
-        
-//            Divider()
-                       Button(action: {
-                           withAnimation {
-                               showPersonalPage.toggle()
-                           }
-                       }) {
-                           HStack {
-                               Text("Profile")
-                               Spacer()
-                               Image(systemName: showPersonalPage ? "chevron.down" : "chevron.right")
-                           }
-                           .padding()
-                       }
-
-                 
-                       if showPersonalPage {
-                           ZStack(alignment: .trailing) {
-                               // TextField for name input
-                               TextField("Enter your name", text: $name)
-                                   .textFieldStyle(RoundedBorderTextFieldStyle())
-                                   .padding(.leading)
-
-                               
-                               if !name.isEmpty {
-                                   Button(action: {
-                            // Clear the text in the text field when the button is clicked
-                                       
-                                       name = ""
-                                   }) {
-                                       Image(systemName: "xmark.circle.fill")
-                                           .foregroundColor(.gray)
-                                           .padding(.trailing, 10)
-                                   }
-                               }
-                           }
-                           .padding(.horizontal)
-                           .transition(.slide)
-                           .padding(.bottom, 10)
-                       }
-
-                  Divider()
-                
-                Toggle(isOn: $notificationsOn) {
-                    Text("Notifications")
-                }
-                .padding()
-                
-                Divider()
-                
-                Button(action: {
-                    withAnimation {
-                        showAboutUs.toggle()
-                    }
-                }) {
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading) {
                     HStack {
-                        Text("About Us")
+                        NavigationLink(destination: Schedule()) {
+                            Image(systemName: "chevron.left")
+                                .foregroundColor(.blue)
+                            Text("schedule")
+                                .foregroundColor(.blue)
+                        }
                         Spacer()
-                        Image(systemName: showAboutUs ? "chevron.down" : "chevron.right")
+                    }
+                   //.padding(.top)
+                    .padding(.horizontal)
+                    
+            
+                    Text("Sitting")
+                        .font(.system(size: 40, weight: .bold))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.horizontal)
+                    
+                    Divider()
+                    
+                        Button(action: {
+                        withAnimation {
+                            showPersonalPage.toggle()
+                        }
+                    }) {
+                        HStack {
+                            Text("Profile")
+                            Spacer()
+                            Image(systemName: showPersonalPage ? "chevron.down" : "chevron.right")
+                        }
+                        .padding()
+                    }
+                    
+                    if showPersonalPage {
+                        ZStack(alignment: .trailing) {
+                            TextField("Enter your name", text: $name)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                                .padding(.leading)
+                            
+                            if !name.isEmpty {
+                                Button(action: {
+                                    name = ""
+                                }) {
+                                    Image(systemName: "xmark.circle.fill")
+                                        .foregroundColor(.gray)
+                                        .padding(.trailing, 10)
+                                }
+                            }
+                        }
+                        .padding(.horizontal)
+                        .transition(.slide)
+                        .padding(.bottom, 10)
+                    }
+                    
+                    Divider()
+                    
+                    // Toggle الإشعارات لازم اربطها مع الاشعارات
+                    Toggle(isOn: $notificationsOn) {
+                        Text("Notifications")
                     }
                     .padding()
+                    
+                    Divider()
+                    
+                 
+                    Button(action: {
+                        withAnimation {
+                            showAboutUs.toggle()
+                        }
+                    }) {
+                        HStack {
+                            Text("About Us")
+                            Spacer()
+                            Image(systemName: showAboutUs ? "chevron.down" : "chevron.right")
+                        }
+                        .padding()
+                    }
+                    
+                    if showAboutUs {
+                        Text("We are a Titan Team")
+                            .padding(.leading)
+                            .transition(.slide)
+                    }
+                    
+                    Divider()
                 }
-                if showAboutUs {
-                    Text("We are a Titan Team ")
-                        .padding(.leading)
-                        .transition(.slide)
-                }
-                
-                Divider()
+                .padding(.top)
             }
         }
-
-
+    }
+}
 
 #Preview {
     Sitting()
 }
+//.navigationBarBackButtonHidden(true)
