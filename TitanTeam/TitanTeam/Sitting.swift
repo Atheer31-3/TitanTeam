172aeaffc9400263419a8211ec5f2dc569f99e69
Atheer Alshareef <105993810+Atheer31-3@users.noreply.github.com>
2024-10-01 16:39:26 +0300

    sitting

--- TitanTeam/TitanTeam/Sitting.swift
+++ TitanTeam/TitanTeam/Sitting.swift
@@ -1,17 +1,94 @@
-//
-//  Sitting.swift
-//  TitanTeam
-//
-//  Created by atheer alshareef on 27/03/1446 AH.
-//
 
 import SwiftUI
 
 struct Sitting: View {
+    @State private var showPersonalPage: Bool = false
+    @State private var notificationsOn: Bool = false
+    @State private var showAboutUs: Bool = false
+    @State private var name: String = ""
+//    let scrums: [Sitting]
+    
     var body: some View {
-        Text("Hello, as!")
+        VStack(alignment: .leading) {
+            Text("Settings")
+                .font(.largeTitle)
+                .bold()
+                .padding(.top, 200)
+                .padding(.leading)
+            
+//            Divider()
+                       Button(action: {
+                           withAnimation {
+                               showPersonalPage.toggle()
+                           }
+                       }) {
+                           HStack {
+                               Text("Profile")
+                               Spacer()
+                               Image(systemName: showPersonalPage ? "chevron.down" : "chevron.right")
+                           }
+                           .padding()
+                       }
+
+                 
+                       if showPersonalPage {
+                           ZStack(alignment: .trailing) {
+                               // TextField for name input
+                               TextField("Enter your name", text: $name)
+                                   .textFieldStyle(RoundedBorderTextFieldStyle())
+                                   .padding(.leading)
+
+                               
+                               if !name.isEmpty {
+                                   Button(action: {
+                            // Clear the text in the text field when the button is clicked
+                                       
+                                       name = ""
+                                   }) {
+                                       Image(systemName: "xmark.circle.fill")
+                                           .foregroundColor(.gray)
+                                           .padding(.trailing, 10)
+                                   }
+                               }
+                           }
+                           .padding(.horizontal)
+                           .transition(.slide)
+                           .padding(.bottom, 10)
+                       }
+
+                  Divider()
+                
+                Toggle(isOn: $notificationsOn) {
+                    Text("Notifications")
+                }
+                .padding()
+                
+                Divider()
+                
+                Button(action: {
+                    withAnimation {
+                        showAboutUs.toggle()
+                    }
+                }) {
+                    HStack {
+                        Text("About Us")
+                        Spacer()
+                        Image(systemName: showAboutUs ? "chevron.down" : "chevron.right")
+                    }
+                    .padding()
+                }
+                if showAboutUs {
+                    Text("We are a Titan Team ")
+                        .padding(.leading)
+                        .transition(.slide)
+                }
+                
+                Divider()
+            }
+            .padding(.horizontal)
+        }
     }
-}
+
 
 #Preview {
     Sitting()
